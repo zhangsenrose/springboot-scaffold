536b23a14717c46714d39760678413cd62f5df4f
zhangsw <zhangsenrose@163.com>
2020-01-03 17:16:40 +0800

    Initial commit

---
+++ SpringbootScaffoldApplication.java
@@ -0,0 +1,13 @@
+package com.cyborg.cn.springbootscaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootScaffoldApplication.class, args);
+    }
+
+}
---
+++ config/MyBatisConfig.java
@@ -0,0 +1,7 @@
+package com.cyborg.cn.springbootscaffold.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisConfig {
+}
